0d02186b17551fcc54d6f05526e1dd063a7d50c1
FacundoL <laugledev@gmail.com>
2022-10-30 11:39:46 -0300

    cambio de archivos

---
+++ desafios/mda.js
@@ -0,0 +1,11 @@
+/* const fs = require('fs'); */
+
+class Contenedor {
+    constructor(nombre){
+        this.nombre = nombre;
+    }
+}
+
+const contenido = new Contenedor('Facundo');
+
+console.log(contenido);
