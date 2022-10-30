f85aa799540aeea8e7471dd74b4d9396a282549c
FacundoL <laugledev@gmail.com>
2022-10-30 12:04:16 -0300

    modificiacion constructor

--- desafios/mda.js
+++ desafios/mda.js
@@ -1,11 +1,25 @@
-/* const fs = require('fs'); */
+const fs = require('fs');
 
 class Contenedor {
     constructor(nombre){
         this.nombre = nombre;
     }
+    
+    async save(obj){
+        try {
+            await fs.promises.writeFile(`${this.nombre}.txt`, 'utf-8')
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
 
-const contenido = new Contenedor('Facundo');
+const contenido = new Contenedor('productos')
 
-console.log(contenido);
+console.log(contenido.save());
+
+/* let obj = [
+    {id:1, title:'celular', price:3540},
+    {id:2, title:'celular', price:3540},
+    {id:3, title:'celular', price:3540}
+] */
